2c4a22476b72f7becd5a6ce579593162736e0447
Felix Fung <felix.the.cheshire.cat@gmail.com>
2012-11-28 16:22:45 +0900

    Conservative Lion Setup Script

---
+++ scripts/lion-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+ln -s $DIR/../vimrc ~/.vimrc
+ln -s $DIR/../aliases ~/.aliases
+ln -s $DIR/../osx ~/.osx
+ln -s $DIR/../gitconfig ~/.gitconfig
+ln -s $DIR/../gitignore ~/.gitignore
